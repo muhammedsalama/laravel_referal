b35fc922bb97de9be9e4eed86bc1236fd1fc7e46
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-30 14:18:39 +0200

    Referral Level and Referral Category model and migration

---
+++ app/Ref_Category.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ref_Category extends Model
+{
+    //
+}
---
+++ app/Ref_Level.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ref_Level extends Model
+{
+    //
+}
