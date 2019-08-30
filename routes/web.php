4d149381f325375a6c8602de4ba054764753e7dd
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-30 15:22:00 +0200

    Install LTE Admin panel and publish layout

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
